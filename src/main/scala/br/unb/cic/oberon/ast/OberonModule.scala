278264f8c10307ff40262d7f92e407c6da0fb2ef
João Víctor Siqueira <71791580+StrawHat972@users.noreply.github.com>
2021-04-08 10:09:41 -0300

    Update OberonModule.scala

--- src/main/scala/br/unb/cic/oberon/ast/OberonModule.scala
+++ src/main/scala/br/unb/cic/oberon/ast/OberonModule.scala
@@ -50,6 +50,9 @@ abstract class Value[T](val value: T) extends Expression
 case class Brackets(exp: Expression) extends Expression
 case class IntValue(v: Int) extends Value[Int](v)
 case class RealValue(v: Float) extends Value[Float](v)
+case class ShortValue(v: Short) extends Value[Short](v)
+case class LongIntValue(v: Long) extends Value[Long](v)
+case class LongRealValue(v: Double) extends Value[Double](v)
 case class BoolValue(v: Boolean) extends Value[Boolean](v)
 case class ArrayValue(v: List[Expression]) extends Value[List[Expression]](v)
 case class ArraySubscript(arrayBase: Expression, index: Expression) extends Expression
@@ -112,6 +115,9 @@ trait Type {
 
 case object IntegerType extends Type
 case object RealType extends Type
+case object ShortType extends Type
+case object LongIntType extends Type
+case object LongRealType extends Type
 case object BooleanType extends Type
 case object UndefinedType extends Type
 case class ReferenceToUserDefinedType(name: String) extends Type
